5de118eca892d82ffada084978862fb52f1a169b
Miguel Ángel Lozano <25maikelo@gmail.com>
2022-08-09 09:20:23 -0500

    WIP: Fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
     'react/prop-types': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-props-no-spreading': 'off',
+    'react/jsx-one-expression-per-line': 'off',
     'import/prefer-default-export': 'off',
     'no-debugger': 1,
     'react/function-component-definition': [
